20eff248bd54ce16519af5ded31e6244f011c3c1
gusdyd98 <gusdyd98@gmail.com>
2021-10-03 16:03:46 +0900

    59page studied 62page studying

--- chapter1_study.py
+++ chapter1_study.py
@@ -25,3 +25,28 @@ print(reserve_tb.loc[:, ['reserve_id', 'hotel_id', 'customer_id',
 
 print(reserve_tb.drop(['people_num', 'total_price'], axis=1, inplace=True))
 
+print("############################################")
+print("##  59page. 조건에 맞는 행 추출                ")
+print("############################################")
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+print(reserve_tb[(reserve_tb['checkout_date'] >= '2016-10-13') &
+           (reserve_tb['checkout_date'] <= '2016-10-14')])
+
+print("------------------------")
+print(reserve_tb.loc[(reserve_tb['checkout_date'] >= '2016-10-13') &
+                 (reserve_tb['checkout_date'] <= '2016-10-14'), :])
+
+print("------------------------")
+print(reserve_tb.query(' "2016-10-13" <= checkout_date <= "2016-10-14" '))
+
+
+print("############################################")
+print("##  62page. 데이터 값을 고려하지 않는 샘플링             ")
+print("############################################")
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
